935187cdf1def0cd254a89ba38e72f59a488f5d2
Domarys <domaryscorrea@gmail.com>
2019-04-26 19:12:22 -0300

    Adicionada novas funções, ainda requer ajustes.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <learnopengl/model.h>
 
 #include <iostream>
+#include <glm/ext.hpp>
 
 //headers
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); //atulização do tamanho da janela
@@ -87,7 +88,12 @@ int main()
 
     // load models
     // -----------
-    Model ourModel(FileSystem::getPath("resources/objects/nanosuit/nanosuit.obj"));
+	//vector<Model> ourModel;
+    Model ourModel(FileSystem::getPath("resources/objects/cyborg/cyborg.obj"));
+	//Model ourModel(FileSystem::getPath("resources/objects/nanosuit/nanosuit.obj"));
+	//Model ourModel(FileSystem::getPath("resources/objects/planet/planet.obj"));
+	//Model ourModel(FileSystem::getPath("resources/objects/rock/rock.obj"));
+
 
     
     // draw in wireframe
@@ -109,7 +115,12 @@ int main()
 	glm::mat4 view = matrixCam[0].GetViewMatrix();
 	ourShader.setMat4("projection", projection);
 	ourShader.setMat4("view", view);
-	float bez = 0.0;
+	float tmp = 0.0,tropeco = 0;
+
+	glm::mat4 aux;
+
+	float radius = 10.0f;
+	int sinal = 0;
 
 
     // render loop
@@ -124,6 +135,7 @@ int main()
 		lastFrame = currentFrame;
 
 		currentTime = glfwGetTime();
+
 
 		// input
 		// -----
@@ -140,11 +152,11 @@ int main()
 		// view/projection transformations
 		projection= glm::perspective(glm::radians(matrixCam[idCam].Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		view = matrixCam[idCam].GetViewMatrix();
-		std::cout << "camera:" << idCam << endl;
-
-
+		//std::cout << "camera:" << idCam << endl;
+		
 		ourShader.setMat4("projection", projection);
-		ourShader.setMat4("view", view);
+		ourShader.setMat4("view", view);		
+		//ourShader.setMat4("view", view);
 
 		//for (int i = 0; i < matrixModel.size(); i++) {
 			//ourShader.setMat4("model", matrixModel[i]);
@@ -257,11 +269,9 @@ int main()
 
 		if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) { 		//animacao????????
 
-
 			double step, delta, tempoDefinido = 5, tempoAtual, tempoInicial = glfwGetTime();
 
 			do {
-
 				tempoAtual = glfwGetTime();
 				delta = tempoAtual - tempoInicial;
 				step = (0.002 * delta) / tempoDefinido;
@@ -278,7 +288,6 @@ int main()
 				glfwPollEvents(); // verifica a ocorrência de interações do usuário com a janela
 
 			} while (tempoAtual <= (tempoInicial + tempoDefinido));
-
 		}
 		
 		if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS && (currentTime-lastTime) > 1) { //new
@@ -313,37 +322,49 @@ int main()
 
 		}
 
-		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && (currentTime - lastTime) > 1) {
+		if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && (currentTime - lastTime) > 1) { // bspline
 
-				if (bez <= 1.0) {
+			if (tmp <= 1.0) {
 					glm::vec3 b = glm::catmullRom(
 						glm::vec3(matrixModel[idModel][0][0], matrixModel[idModel][1][1] - 5, matrixModel[idModel][2][2]),
-						glm::vec3(matrixModel[idModel][0][0], matrixModel[idModel][1][1], matrixModel[idModel][2][0]),
+						glm::vec3(matrixModel[idModel][0][0], matrixModel[idModel][1][1], matrixModel[idModel][2][2]),
 						glm::vec3(matrixModel[idModel][0][0] + 1.0, matrixModel[idModel][1][1], matrixModel[idModel][2][2]),
 						glm::vec3(matrixModel[idModel][0][0], matrixModel[idModel][1][1], matrixModel[idModel][2][2]),
-						bez);
+						tmp);
 					matrixModel[idModel][3][0] = b.x;
 					matrixModel[idModel][3][1] = b.y;
 					matrixModel[idModel][3][2] = b.z;
-					bez += 0.001;
+					tmp += 0.001;
 				}
 				else {
-					bez = 0.0;
+				tmp = 0.0;
 				}
+		}
+		if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS) { // bezier
+
+			glm::vec3 a(2.5f, -3.0f, 0.0f),	b(0.0f, 0.0f, 0.0f), c(5.0f, -2.3f, -1.0f),	d(3.5, 3.0f, -1.0f);
+
+			if (tmp <= 1.0f) {
+				glm::vec3 ponto = (float)pow(1 - tmp, 3) * a +
+					3 * (float)pow(1 - tmp, 2) * tmp * b +
+					3 * (1 - tmp) * (float)pow(tmp, 2) * c +
+					(float)pow(tmp, 3) * d;
+				matrixModel[idModel][3][0] = ponto.x;
+				matrixModel[idModel][3][1] = ponto.y;
+				matrixModel[idModel][3][2] = ponto.z;
+				tmp += 0.001;
+			} else {
+				tmp = 0.0;
+			}
+
 
 		}
-		if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS) {
-
-
-		}
-
+		// CAMERA
 		if (glfwGetKey(window, GLFW_KEY_F1) == GLFW_PRESS  && (currentTime - lastTime) > 1) { //new cam
-
 			Camera cam = glm::vec3(0.0f, 0.0f, 3.0f);
 			matrixCam.push_back(cam);
 			lastTime = currentTime;
 			idCam = (matrixCam.size() - 1);
-
 		}
 
 		if (glfwGetKey(window, GLFW_KEY_F2) == GLFW_PRESS && (currentTime - lastTime) > 1) { //idCam ++
@@ -365,21 +386,151 @@ int main()
 			}
 			lastTime = currentTime;
 		}
-		if (glfwGetKey(window, GLFW_KEY_F4) == GLFW_PRESS && (currentTime - lastTime) > 1) {// translacao -x 
-			//glm::mat4 aux(matrixCam[idCam].Position.x, 0, 0, 0, 0, matrixCam[idCam].Position.y, 0, 0, 0, 0, matrixCam[idCam].Position.z, 0, 0, 0, 0, 1);
-			glm::mat4 aux;
-			aux[3][0] = matrixCam[idCam].Position.x;
-			aux[3][1] = matrixCam[idCam].Position.y;
-			aux[3][2] = matrixCam[idCam].Position.z;;
 
-			aux = glm::translate(aux, glm::vec3(-0.2f, 0.0f, 0.0f));
-			matrixCam[idCam].Position.x = aux[3][0];
-			matrixCam[idCam].Position.y = aux[3][1];
-			matrixCam[idCam].Position.z = aux[3][2];
-			lastTime = currentTime;
-
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: translacao +y 			
+			float MovementSpeed = 1;
+			deltaTime = 0.01;
+			float velocity = MovementSpeed * deltaTime;			
+			matrixCam[idCam].Position += matrixCam[idCam].Front * velocity;
 		}
 
+		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: translacao -y			
+			float MovementSpeed = 1;
+			deltaTime = 0.01;
+			float velocity = MovementSpeed * deltaTime;
+			matrixCam[idCam].Position -= matrixCam[idCam].Front * velocity;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: translacao +x 			
+			float MovementSpeed = 1;
+			deltaTime = 0.01;
+			float velocity = MovementSpeed * deltaTime;
+			matrixCam[idCam].Position += matrixCam[idCam].Right * velocity;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: translacao -y		
+			float MovementSpeed = 1;
+			deltaTime = 0.01;
+			float velocity = MovementSpeed * deltaTime;
+			matrixCam[idCam].Position -= matrixCam[idCam].Right * velocity;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F4) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: rotação eixo 	
+			glm::vec3 dotMod(1.0f, 0.0f, 0.0f); //cameraTarget
+			glm::vec3 vecAux;
+
+			vecAux[0] = matrixModel[idModel][3][0];
+			vecAux[1] = matrixModel[idModel][3][1];
+
+			vecAux[2] = matrixModel[idModel][3][2];
+			glm::vec3 direction = glm::normalize(dotMod - (0.0f, -1.0f, 0.0f));
+			std::cout << "Angulo diferenca camera e modelo: " << std::endl;
+			std::cout << "X: " << direction.x << " " << "Y: " << direction.y << " " << "Z: " << direction.z << std::endl;
+
+			float camX = sin(glfwGetTime()) * direction.z;
+			float camZ = cos(glfwGetTime()) * direction.z;
+			glm::mat4 view;
+			view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+			ourShader.setMat4("projection", projection);
+			ourShader.setMat4("view", view);		
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F5) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: zoom		
+			if (matrixCam[idCam].Zoom >= 1.0f && matrixCam[idCam].Zoom <= 45.0f)
+				matrixCam[idCam].Zoom -= 1;
+			if (matrixCam[idCam].Zoom <= 1.0f)
+				matrixCam[idCam].Zoom = 1.0f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F6) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: retirada zoom	
+			if (matrixCam[idCam].Zoom >= 1.0f && matrixCam[idCam].Zoom <= 45.0f)
+				matrixCam[idCam].Zoom += 1;
+			if (matrixCam[idCam].Zoom >= 45.0f)
+				matrixCam[idCam].Zoom = 45.0f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F7) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: lookat	
+			float camX = sin(glfwGetTime()) * 10;
+			float camZ = cos(glfwGetTime()) * 10;
+			glm::mat4 view;
+			view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 1.0), glm::vec3(0.0, 1.0, 0.0));
+			ourShader.setMat4("projection", projection);
+			ourShader.setMat4("view", view);
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F8) == GLFW_PRESS && (currentTime - lastTime) > 1) { // CAM: spline
+			if (tmp <= 1.0) {
+				glm::vec3 b = glm::catmullRom(
+					glm::vec3(2.0f, 1.0f, 0.0f),
+					glm::vec3(0.0f, -0.8f, 0.0f),
+					glm::vec3(2.5f, -2.3f, -1.0f), 
+					glm::vec3(2.5, 1.2f, -1.0f),
+					tmp);
+				matrixCam[idCam].Position.x = b.x;
+				matrixCam[idCam].Position.y = b.y;
+				matrixCam[idCam].Position.z = b.z;
+				tmp += 0.001;
+			}
+			else {
+				tmp = 0.0;
+			}
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F9) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: bezier
+			glm::vec3 a(2.5f, -3.0f, 0.0f), b(0.0f, 0.0f, 0.0f), c(5.0f, -2.3f, -1.0f), d(3.5, 3.0f, -1.0f);
+
+			if (tmp <= 1.0f) {
+				glm::vec3 ponto = (float)pow(1 - tmp, 3) * a +
+					3 * (float)pow(1 - tmp, 2) * tmp * b +
+					3 * (1 - tmp) * (float)pow(tmp, 2) * c +
+					(float)pow(tmp, 3) * d;
+				matrixModel[idModel][3][0] = ponto.x;
+				matrixModel[idModel][3][1] = ponto.y;
+				matrixModel[idModel][3][2] = ponto.z;
+				tmp += 0.001;
+			}
+			else {
+				tmp = 0.0;
+			}
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F10) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: trôpego	
+			float MovementSpeed = 1;
+			deltaTime = 0.01;
+			float velocity = MovementSpeed * deltaTime;
+			matrixCam[idCam].Position += matrixCam[idCam].Front * velocity;
+			std::cout << "velocity: "<< velocity << std::endl;
+
+
+			if (tropeco >= 1.0) {	
+				sinal = -1;			
+			} 
+			if ((tropeco <= -1)|| (tropeco == 0)) {
+				sinal = 1;
+			}
+			if (sinal == 1) {
+				matrixCam[idCam].Position -= matrixCam[idCam].Up * 0.005; // velocity em 0.01 foi mudada para suavizar o tropeço
+				tropeco += 0.01;
+			}
+			if (sinal == -1) {
+				matrixCam[idCam].Position += matrixCam[idCam].Up * 0.005;
+				tropeco -= 0.01;
+			}
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: retirada zoom	
+			if (matrixCam[idCam].Zoom >= 1.0f && matrixCam[idCam].Zoom <= 45.0f)
+				matrixCam[idCam].Zoom += 1;
+			if (matrixCam[idCam].Zoom >= 45.0f)
+				matrixCam[idCam].Zoom = 45.0f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F12) == GLFW_PRESS && (currentTime - lastTime) > 1) {// CAM: retirada zoom	
+			if (matrixCam[idCam].Zoom >= 1.0f && matrixCam[idCam].Zoom <= 45.0f)
+				matrixCam[idCam].Zoom += 1;
+			if (matrixCam[idCam].Zoom >= 45.0f)
+				matrixCam[idCam].Zoom = 45.0f;
+		}
 
 		//printar objetos
 		//ourShader.setMat4("model", model);
@@ -407,6 +558,7 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) // verifica naquele meio de comunicaçao se determinado evento foi acionado
         glfwSetWindowShouldClose(window, true);
+		/*
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
 		matrixCam[idCam].ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
@@ -414,7 +566,7 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
 		matrixCam[idCam].ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-		matrixCam[idCam].ProcessKeyboard(RIGHT, deltaTime);
+		matrixCam[idCam].ProcessKeyboard(RIGHT, deltaTime); */
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
